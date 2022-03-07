c440e729f12b2aa4e3e46bb2cd00a9579e95d135
AbdManian <abd.manian@gmail.com>
2022-03-07 20:46:24 +0330

    Initial import

--- main.cpp
+++ main.cpp
@@ -1,24 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
 int main(void)
@@ -28,7 +7,7 @@ int main(void)
     const int screen_width = 800;
     const int screen_height = 600;
 
-    InitWindow(screen_width, screen_height, "Sample Project with Raylib");
+    InitWindow(screen_width, screen_height, "Bouncing Ball");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -43,7 +22,7 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Raylib is working!", 5, screen_height - 55, 50, LIGHTGRAY);
+            //DrawText("Raylib is working!", 5, screen_height - 55, 50, LIGHTGRAY);
 
             //DrawFPS(10, 10);
 
