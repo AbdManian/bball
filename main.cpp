7261222c98b73aa3d02525a76f7b20ad86106641
AbdManian <abd.manian@gmail.com>
2022-03-07 21:04:46 +0330

    Add basic movement and collision detection

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main(void)
     const int screen_width = 800;
     const int screen_height = 600;
 
+    int ball_x {100};
+    int ball_y {300};
+
+    int vx {2};
+    int vy {-2};
+
     InitWindow(screen_width, screen_height, "Bouncing Ball");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -16,13 +22,36 @@ int main(void)
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 
+    	if (ball_y <= 0) {
+    		vy = vy*-1;
+    	}
+
+    	if (ball_x>screen_width) {
+    		vx *= -1;
+    	}
+
+    	if (ball_y >= screen_height) {
+    		vy = vy*-1;
+    	}
+
+    	if (ball_x <= 0) {
+    		vx = vx*-1;
+    	}
+
+
+    	ball_x = ball_x + vx;
+    	ball_y = ball_y + vy;
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
+            DrawCircle(ball_x, ball_y, 15, RED);
+
             //DrawText("Raylib is working!", 5, screen_height - 55, 50, LIGHTGRAY);
+
+            DrawText(TextFormat("x=%d Y=%d", ball_x, ball_y),20 , screen_height-50, 30 , BLACK);
 
             //DrawFPS(10, 10);
 
